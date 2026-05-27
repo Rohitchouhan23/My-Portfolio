964e28910d06fb27c8892f3adba92cad40916a60
rohitchouhan <rohitchouhan.1811@gmail.com>
2026-05-27 14:47:42 +0530

    updated code

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -4,7 +4,7 @@ import {FaExternalLinkAlt ,FaGithub} from 'react-icons/fa'
 import Project1 from '../assets/project1.png'
 import Project2 from '../assets/project2.png'
 import Project3 from '../assets/Project3.png'
-import Project4 from '../assets/Project4.png'
+import Project4 from '../assets/project4.png'
 function Project({darkmode}) {
   const projects=[
     {
